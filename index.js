bae950395efeeed2f7717f09041c9638ae64721e
Azizbek Rustamov <rystamov2233@gmail.com>
2024-05-25 14:07:25 +0500

    javascript

---
+++ index.js
@@ -0,0 +1,5 @@
+let ism = prompt("ismingizne kiriting");// aziz
+let yosh = prompt("yoshingizne kiriting");// 15
+let mashina = prompt("mashinangiz bormi?");// true
+
+alert(`${ism} ${yosh}da,${mashina}`);// aziz 15da,mashinasi true
